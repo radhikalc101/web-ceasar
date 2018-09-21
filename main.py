8b05b7ae8389a204a3dbcab48b1da74f8a86bee4
Rsdhika <radheram831@gmail.com>
2018-09-21 17:01:11 -0500

    modified

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -9,19 +10,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color:#eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius:10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -31,15 +32,23 @@ form = """
                 <input name="rot" type="text" value="0"/>
             </label>
             <label>
-                <textarea name="text"></textarea>
+                <textarea name="text">{encrypted}</textarea>
             </label>
             <input type="submit" value="Submit Quary"/>
 
     </body>
 </html>
 """
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot_int = int(request.form['rot'])
+    text_str = request.form['text'] 
+    encrypted_string = rotate_string(text_str, rot_int)
+    #return <h1> + encrypted_string + </h1>
+    return form.format(encrypted=encrypted_string) 
+
 @app.route("/")
 def index():
-    return form
+    return form.format(encrypted='')
 
 app.run()
